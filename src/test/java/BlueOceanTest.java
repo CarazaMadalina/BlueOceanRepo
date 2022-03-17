4b3465a127348281dd36d208febb199e4cb518a7
Caraza Madalina Ana <carazamadalinaana@yahoo.ro>
2022-03-18 00:44:24 +0200

    Update BlueOceanTest.java

--- src/test/java/BlueOceanTest.java
+++ src/test/java/BlueOceanTest.java
@@ -8,18 +8,21 @@ public class BlueOceanTest {
 
     @Test
     public void firstTest() {
+        System.out.println("First test");
         long currentTime = System.currentTimeMillis();
         assertTrue("the time is not correct" , currentTime > 0);
     }
 
     @Test
     public void secondTest() {
+        System.out.println("Second test");
         String title = "Jenkins workshop";
         assertTrue("The string does not contain 'Jenkins' word", title.contains("Jenkins"));
     }
 
     @Test
     public void thirdTest() {
+        System.out.println("Third test");
         Random random = new Random();
         int randomNumber = random.nextInt(10);
         assertTrue("The number is 52", randomNumber != 52);
@@ -27,6 +30,7 @@ public class BlueOceanTest {
     
      @Test
     public void fourthTest() {
+        System.out.println("Fourth test");
         Random random = new Random();
         int randomNumber = random.nextInt(10);
         assertTrue("The number is 12", randomNumber != 12);
